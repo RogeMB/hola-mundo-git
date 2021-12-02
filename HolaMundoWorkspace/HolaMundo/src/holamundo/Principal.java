a6f830ff311f2095119c6c1f46698d91b337df4a
Rogelio Mohigefer Barrera <rogelio.mb@hotmail.com>
2021-12-02 09:16:47 +0100

    superbucle de la muerte

--- HolaMundoWorkspace/HolaMundo/src/holamundo/Principal.java
+++ HolaMundoWorkspace/HolaMundo/src/holamundo/Principal.java
@@ -4,7 +4,20 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hola Mundo!");
+		System.out.println("Hola Mundo!");
+
+		int [] array = new int [3];
+		array [0] = 123;
+		array [1] = 456;
+		array [2] = 789;
+
+		System.out.println(array [0]);
+		System.out.println(array [1]);
+		System.out.println(array [2]);
+
+
+
+
 	}
 
 }
