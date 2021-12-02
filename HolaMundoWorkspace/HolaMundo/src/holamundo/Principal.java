50e6e789e2adf66af6409c1d53a3543987e13181
Rogelio Mohigefer Barrera <rogelio.mb@hotmail.com>
2021-12-02 09:26:36 +0100

    nuevos cambios

--- HolaMundoWorkspace/HolaMundo/src/holamundo/Principal.java
+++ HolaMundoWorkspace/HolaMundo/src/holamundo/Principal.java
@@ -4,7 +4,16 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hola Mundo!");
+		System.out.println("Hello World!");
+
+		int [] arr = new int [] {123, 456, 789};
+
+		for(int a: arr) {
+			System.out.println(a);
+		}
+
+
+
 	}
 
 }
